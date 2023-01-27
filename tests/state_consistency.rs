b563af6d781128704df3d3f112e63652d43dc47c
fluiderson <fluidcells@protonmail.ch>
2023-01-27 22:59:21 +0300

    Pin to the `stable` branch, fix metahash including

--- tests/state_consistency.rs
+++ tests/state_consistency.rs
@@ -82,7 +82,13 @@ async fn common_upload_program(
         .await?
         .min_limit;
     let (message_id, program_id, _) = client
-        .upload_program(code, gclient::bytes_now(), payload, gas_limit, 0)
+        .upload_program(
+            code,
+            gclient::now_in_micros().to_le_bytes(),
+            payload,
+            gas_limit,
+            0,
+        )
         .await?;
 
     Ok((message_id.into(), program_id.into()))
@@ -129,7 +135,7 @@ async fn send_message<T: Decode>(
     payload: impl Encode + Debug,
 ) -> Result<T> {
     Ok(
-        send_message_with_custom_limit(client, listener, destination, payload, |gas| gas * 4)
+        send_message_with_custom_limit(client, listener, destination, payload, |gas| gas * 6)
             .await?
             .expect("Received an error message instead of a reply"),
     )
